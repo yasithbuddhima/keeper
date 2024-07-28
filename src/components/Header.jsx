12e5ecaf51980bc48a83248b2606b1685d1b53e0
Yasith Buddhima <yasithbuddhima@gmail.com>
2024-07-28 21:05:10 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WbIncandescentTwoToneIcon from '@mui/icons-material/WbIncandescentTwoTone';
+
+function Header() {
+  return (
+    <header>
+      <h1><WbIncandescentTwoToneIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
